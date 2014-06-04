e2df31d1f0bf5c78d26ec38dd6e21b89e32519a0
Paul Morie <pmorie@gmail.com>
2014-06-03 23:06:06 -0400

    wip: demo

--- contrib/demo-multi/setup-multi.sh
+++ contrib/demo-multi/setup-multi.sh
@@ -23,12 +23,24 @@ $base/teardown-multi.sh
 #gear deploy $base/deploy_parks_map.json localhost localhost 192.168.205.11 192.168.205.11 localhost
 #gear stop 192.168.205.11/parks-backend-{2,3}
 
-gear install -p "27017:4003" openshift/centos-mongodb --start
-gear install -p "3000:4002" -n "127.0.0.1:27017:localhost:4003" parks-map-app parks-backend-1  
-gear install -p "8080:14000" -p "192.168.1.1:8080:localhost:4002,129.168.1.2:8080:192.168.205.11:4002,129.168.1.2:8080:192.168.205.11:4003" parks-lb-1
-gear install -p "3000:4002" -p "127.0.0.1:27017:192.168.205.10:4003" atomic-2/parks-backend-2
-gear install -p "3000:4003" -p "127.0.0.1:27017:192.168.205.10:4003" atomic-2/parks-backend-3
-
+gear install openshift/centos-mongodb parks-db-1 -p "27017:4003" openshift/centos-mongodb --start
 $base/wait_for_url.sh "http://localhost:4003/"
+read
 
-sudo switchns --container=parks-db-1 -- /bin/bash -c "curl https://raw.githubusercontent.com/thesteve0/fluentwebmap/master/parkcoord.json | mongoimport -d fluent -c parkpoints --type json && mongo fluent --eval 'db.parkpoints.ensureIndex( { pos : \"2d\" } );'"
+gear install parks-map-app parks-backend-1 -p "3000:4002" -n "127.0.0.1:27017:localhost:4003" --start
+$base/wait_for_url.sh "http://localhost:4002/"
+read
+
+gear install parks-map-app atomic-2:43273/parks-backend-2 -p "3000:4002" -n "127.0.0.1:27017:192.168.205.10:4003" --start
+$base/wait_for_url.sh "http://atomic-2:4002"
+read
+
+gear install parks-map-app atomic-2:43273/parks-backend-3 -p "3000:4003" -n "127.0.0.1:27017:192.168.205.10:4003" --start
+$base/wait_for_url.sh "http://atomic-2:4003"
+read
+
+gear install openshift/centos-haproxy-simple-balancer parks-lb-1 -p "8080:14000" -n "192.168.1.1:8080:localhost:4002,192.168.1.2:8080:192.168.205.11:4002,192.168.1.3:8080:192.168.205.11:4003" --start
+$base/wait_for_url.sh "http://localhost:14000"
+read
+
+$base/db-import.sh
